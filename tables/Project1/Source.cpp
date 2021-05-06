b98ea4acfe1e091f2962b80effc96572cb68866c
Alexei Zaharov <alexcs2013@mail.ru>
2021-05-06 18:25:39 +0300

    hash 1.0

--- tables/Project1/Source.cpp
+++ tables/Project1/Source.cpp
@@ -5,6 +5,7 @@
 #include <stack>
 #include <vector>
 #include <queue>
+#include <list>
 using namespace std;
 
 //for work with file
@@ -2357,6 +2358,241 @@ public:
 };
 
 
+int Hash(string fullName)
+{
+	if (fullName.length() != 3)
+	{
+		cout << "Ошибка хэш (длина)" << endl;
+		return int();
+	}
+	for (int i = 0; i < fullName.length(); i++)
+	{
+		if (fullName[i] > 'Я' || fullName[i] < 'А')
+		{
+			cout << "Ошибка хэш (не те символы)" << endl;
+			return int();
+		}
+	}
+	//int SIZE = 33 * 33 * 33;
+	int hash;
+	hash = (fullName[0] - 'А')%256 + (fullName[1] - 'А') % 256 *32  + (fullName[2] - 'А') % 256 * 32 * 32;
+	return hash;// % SIZE;
+}
+
+class THashTable : public TScanTable
+{
+	vector<list<string>> hashTable;
+public:
+	THashTable(string path) : TScanTable(path)
+	{
+		hashTable.reserve(Hash("ЯЯЯ") + 1);
+
+		for (int i = 0; i < hashTable.capacity(); i++)
+		{
+			hashTable.push_back(list<string>());
+		}
+
+		string subjects = array[0];
+		for (int i = 0; i < subjects.length(); i++)
+		{
+			if (subjects[i] == '|')
+			{
+				subjects[i] = '\t';
+			}
+		}
+		subjects += '\n';
+
+		string person;	
+		string fullName;
+		
+		for (int i = 1; i < stringCount; i++)
+		{
+			person = subjects;
+			fullName = string();
+			for (int j = 0; j < array[i].length(); j++)
+			{
+				if (array[i][j] == '|')
+				{
+					person.push_back('\t');
+					continue;
+				}
+				if (array[i][j] >= 'А' && array[i][j] <= 'Я')
+				{
+					fullName.push_back(array[i][j]);
+				}
+				person.push_back(array[i][j]);
+			}
+
+			hashTable[Hash(fullName)].push_front(person);
+		}
+
+	}
+
+	string finder(string key) override
+	{
+		int goNext = 0;
+		if (key[0] >= '0' && key[0] <= '9')
+		{
+			string help = string();
+			help.push_back(key[0]);
+			goNext = stoi(help);
+			key.erase(0, 1);
+		}
+
+		string new_key = string();
+		for (int i = 0; i < key.length(); i++)
+		{
+			if (key[i] >= 'А' && key[i] <= 'Я')
+			{
+				new_key.push_back(key[i]);
+			}
+		}
+
+		int index = Hash(new_key);
+		if (hashTable[index].size() == 0)
+		{
+			cout << "Нет такого ФИО" << endl;
+			return string();
+		}
+		int index_in_list = 0;
+		while (index_in_list < hashTable[index].size())
+		{
+			if ((*next(hashTable[index].begin(), index_in_list)).find(key) != string::npos)
+			{
+				if (goNext > 0)
+				{
+					goNext--;
+					index_in_list++;
+					continue;
+				}
+				cout << *next(hashTable[index].begin(), index_in_list) << endl;
+				return *next(hashTable[index].begin(), index_in_list);
+			}
+			index_in_list++;
+		}
+		cout << "Нет такого ФИО" << endl;
+		return string();
+	}
+
+	bool deleter(string key) override
+	{
+		int goNext = 0;
+		if (key[0] >= '0' && key[0] <= '9')
+		{
+			string help = string();
+			help.push_back(key[0]);
+			goNext = stoi(help);
+			key.erase(0, 1);
+		}
+
+		string new_key = string();
+		for (int i = 0; i < key.length(); i++)
+		{
+			if (key[i] >= 'А' && key[i] <= 'Я')
+			{
+				new_key.push_back(key[i]);
+			}
+		}
+
+		int index = Hash(new_key);
+		if (hashTable[index].size() == 0)
+		{
+			cout << "Нет такого ФИО" << endl;
+			return false;
+		}
+		int index_in_list = 0;
+		while (index_in_list < hashTable[index].size())
+		{
+			if ((*next(hashTable[index].begin(), index_in_list)).find(key) != string::npos)
+			{
+				if (goNext > 0)
+				{
+					goNext--;
+					index_in_list++;
+					continue;
+				}
+				//cout << *next(hashTable[index].begin(), index_in_list) << endl;
+				hashTable[index].erase(next(hashTable[index].begin(), index_in_list));
+				TScanTable::deleter(key);
+				return true;
+			}
+			index_in_list++;
+		}
+		cout << "Нет такого ФИО" << endl;
+		return false;
+	}
+
+	void inserter(string person, string marks) override
+	{
+		int number_of_marks = 0;
+		for (int i = 0; i < array[0].length(); i++)
+		{
+			if (array[0][i] == '|')
+			{
+				number_of_marks++;
+			}
+		}
+
+		if (marks.length() / 2 + 1 != number_of_marks)
+		{
+			cout << "Ошибка: неправильное кол-во оценок. Кол-во = "<<number_of_marks << endl;
+			return void();
+		}
+
+		string subjects = array[0];
+		for (int i = 0; i < subjects.length(); i++)
+		{
+			if (subjects[i] == '|')
+			{
+				subjects[i] = '\t';
+			}
+		}
+		subjects += '\n';
+
+		string fullName = string();
+		for (int i = 0; i < person.length(); i++)
+		{
+			if (person[i] >= 'А' && person[i] <= 'Я')
+			{
+				fullName.push_back(person[i]);
+			}
+		}
+
+		if (fullName.length() != 3)
+		{
+			cout << "Неправильное ФИО" << endl;
+			return void();
+		}
+
+		string truePerson = subjects + person;
+		truePerson += '\t';
+		for (int i = 0; i < marks.length(); i++)
+		{
+			if (marks[i] == ' ')
+			{
+				truePerson.push_back('\t');
+				continue;
+			}
+			truePerson.push_back(marks[i]);
+		}
+
+
+		hashTable[Hash(fullName)].push_front(truePerson);
+
+		string marks_to_scanTable = string();
+		marks_to_scanTable.push_back('|');
+		for (int i = 0; i < marks.length(); i++)
+		{
+			if (marks[i] == ' ')
+			{
+				marks_to_scanTable.push_back('|');
+				continue;
+			}
+			marks_to_scanTable.push_back(marks[i]);
+		}
+		TScanTable::inserter(person+ marks_to_scanTable, "line");
+	}
+};
 
 
 int main()
@@ -2367,15 +2603,76 @@ int main()
 
 	string path = "oldTable.txt";
 
-	TTreeNode table3(path);
 
-	//table3.inserter("alex", "1 2 3 ");
-	cout<<table3.finder("Ааранов А.А.")<<endl;
-	table3.deleter("Ааранов А.А.");
-	cout << table3.finder("Ааранов А.А.") << endl;
+	THashTable table4(path);
+	table4.inserter("Антон А.И.", "4 4 4");
+	table4.finder("Антон А.И.");
+	//table4.finder("1Захаров А.А.");
+	//table4.deleter("1Захаров А.А.");
+	//table4.finder("0Захаров А.А.");
+	
+	/*list<int> a;
+	a.push_back(1);
+	a.push_back(2);
+	a.erase(next(a.begin(), 1));*/
 
-	cout << endl << endl;
-	table3.show();
+
+	/*string str = "Захар А.А.";
+	if (str.find("Gахаов А.А.") != string::npos)
+	{
+		cout << "yes" << endl;
+	}*/
+	//table4.printArray();
+
+	/*list<string> b;
+	if (*b.begin() == string())
+	{
+		cout << "yes" << endl;
+	}*/
+	//b.push_back("aaa");
+	//b.push_back("bbb");
+	//b.push_back("ccc");
+	//cout << *next(b.begin(), 2);
+
+	/*vector<list<string>> a = 3;
+	if (*a[0].begin() == "")
+	{
+		cout << "yes";
+	}
+	cout << *a[0].begin();*/
+
+	/*vector<int> a;
+	a.reserve(3);
+	cout << a.capacity() << endl;
+	for (int i = 0; i < a.capacity(); i++)
+	{
+		a.push_back(NULL);
+	}
+	if (a[2] == NULL)
+	{
+		cout << "yes";
+		a[2] = 3;
+	}
+	if (a[2] == NULL)
+	{
+		cout << "yes";
+		a[2] = NULL;
+	}*/
+
+	//cout<<"res " << a[0] << " " << a[1];
+
+	//cout << "H " << Hash("ААА")<< endl;
+	//cout << "H " << Hash("ЯЯЯ") << endl;
+	//cout << toupper('а') - 'А';
+	//TTreeNode table3(path);
+
+	////table3.inserter("alex", "1 2 3 ");
+	//cout<<table3.finder("Ааранов А.А.")<<endl;
+	//table3.deleter("Ааранов А.А.");
+	//cout << table3.finder("Ааранов А.А.") << endl;
+
+	//cout << endl << endl;
+	//table3.show();
 
 
 	//cout<< table3.finder("Баранов А.А.") <<endl;
